7aead96f109422de9c09a5e9599aff0df07ec581
Aidas Klimas <aidaskk@gmail.com>
2014-04-12 17:08:17 +0300

    fix: file drop propagation is not needed
    might be a bc, but can be solved by setting onDropStopPropagation prop

--- src/flow.js
+++ src/flow.js
@@ -81,7 +81,8 @@
       generateUniqueIdentifier: null,
       maxChunkRetries: 0,
       chunkRetryInterval: null,
-      permanentErrors: [404, 415, 500, 501]
+      permanentErrors: [404, 415, 500, 501],
+      onDropStopPropagation: false
     };
 
     /**
@@ -106,7 +107,9 @@
      * @param {MouseEvent} event
      */
     this.onDrop = function (event) {
-      event.stopPropagation();
+      if ($.opts.onDropStopPropagation) {
+        event.stopPropagation();
+      }
       event.preventDefault();
       var dataTransfer = event.dataTransfer;
       if (dataTransfer.items && dataTransfer.items[0] &&
