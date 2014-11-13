5dd29c175d86191b9f1f52312372556057183771
Tim Whitman <timothy.whitman@hgst.com>
2014-11-13 12:31:12 -0800

    Support files with empty names when pasting image

--- src/flow.js
+++ src/flow.js
@@ -264,7 +264,7 @@
         return custom(file);
       }
       // Some confusion in different versions of Firefox
-      var relativePath = file.relativePath || file.webkitRelativePath || file.fileName || file.name;
+      var relativePath = file.relativePath || file.webkitRelativePath || file.fileName || file.name || 'pasted';
       return file.size + '-' + relativePath.replace(/[^0-9a-zA-Z_-]/img, '');
     },
 
@@ -692,6 +692,12 @@
     this.name = file.fileName || file.name;
 
     /**
+     * Placeholder for Files with empty names
+     * @type {string}
+     */
+    this.generatedFileName = '';
+
+    /**
      * File size
      * @type {number}
      */
@@ -993,6 +999,21 @@
     },
 
     /**
+     * Generate a name if empty (i.e. paste)
+     * @function
+     */
+    generateFileName: function () {
+      var
+        d = new Date(),
+        ts = Date.parse(d),
+        name = ts + '-generated-filename.png';
+
+      this.generatedFileName = name;
+
+      return this.generatedFileName;
+    },
+
+    /**
      * Get file type
      * @function
      * @returns {string}
@@ -1008,7 +1029,16 @@
      */
     getExtension: function () {
       return this.name.substr((~-this.name.lastIndexOf(".") >>> 0) + 2).toLowerCase();
+    },
+
+    /**
+     * Get File name
+     * @function
+     */
+    getFileName: function () {
+      return this.name || this.generatedFileName || this.generateFileName();       
     }
+
   };
 
 
@@ -1187,7 +1217,7 @@
         flowCurrentChunkSize: this.endByte - this.startByte,
         flowTotalSize: this.fileObjSize,
         flowIdentifier: this.fileObj.uniqueIdentifier,
-        flowFilename: this.fileObj.name,
+        flowFilename: this.fileObj.getFileName(),
         flowRelativePath: this.fileObj.relativePath,
         flowTotalChunks: this.fileObj.chunks.length
       };
@@ -1390,7 +1420,7 @@
         each(query, function (v, k) {
           data.append(k, v);
         });
-        data.append(this.flowObj.opts.fileParameterName, blob, this.fileObj.file.name);
+        data.append(this.flowObj.opts.fileParameterName, blob, this.fileObj.getFileName());
       }
 
       this.xhr.open(method, target, true);
