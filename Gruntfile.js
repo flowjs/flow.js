bcfc3f99d2bfabafaed7701e1f9228c1dc45a567
Aidas Klimas <aidaskk@gmail.com>
2016-01-19 13:53:04 +0200

    fix release command bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,7 +83,7 @@ module.exports = function(grunt) {
     },
     bump: {
       options: {
-        files: ['package.json', 'bower.json'],
+        files: ['package.json'],
         updateConfigs: ['pkg'],
         commit: true,
         commitMessage: 'Release v%VERSION%',
