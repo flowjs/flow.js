b3a42e5a237ba09156e007940336dfa77fd52b05
Raphaël Droz <raphael.droz@gmail.com>
2021-01-21 11:30:35 -0300

    Provide an instrumented build intended for code-coverage (#335)
    Instrumented build so that src/* is covered instead of dist/*

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
+import istanbul from 'rollup-plugin-istanbul';
 import pkg from './package.json';
 import replace from '@rollup/plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
@@ -36,6 +37,17 @@ export default [
             file: pkg.browser.replace(/js$/, 'min.js'),
             sourcemap: true,
             format: 'esm',
+	},
+    },
+    {
+        input: 'src/Flow.js',
+        plugins: plugins.concat([istanbul({exclude: ['node_modules/**/*.js']})]),
+        output: {
+            name: 'Flow',
+            banner: `/*! ${pkg.name} ${pkg.version} */\n`,
+            file: pkg.browser.replace(/js$/, 'cov.js'),
+            sourcemap: true,
+            format: 'umd',
 	},
     }
 
